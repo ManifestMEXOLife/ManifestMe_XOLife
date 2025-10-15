e9f5a5105755408b7f0f0eb5c3898223a69d5731
Rocket3012 <145717198+Rocket3012@users.noreply.github.com>
2025-10-15 13:32:52 -0600

    Convert PORT to a number from environment variable

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 dotenv.config()
 import app from "./app"
 
-const PORT = process.env.PORT || 4000
+const PORT = Number(process.env.PORT) || 4000
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`)
 })
