408f6186ad45ae9a344399c20f30955161ceea4a
Rocket3012 <145717198+Rocket3012@users.noreply.github.com>
2025-10-15 13:59:49 -0600

    Change server port to 8080

--- server.ts
+++ server.ts
@@ -2,7 +2,5 @@ import dotenv from "dotenv"
 dotenv.config()
 import app from "./app"
 
-const PORT = Number(process.env.PORT) || 4000
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 8080
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`))
