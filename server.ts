afdf182ec0cfa39e40fa7b368e956056659f9eea
Rocket3012 <145717198+Rocket3012@users.noreply.github.com>
2025-10-15 14:38:02 -0600

    Add type annotation for port variable

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./app";
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
