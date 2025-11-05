3d7ff27322f30748f138b8ce5b6b40b6bf1e90a6
Rocket3012 <145717198+Rocket3012@users.noreply.github.com>
2025-11-05 12:15:50 -0700

    Create HomePage component with DailyAffirmationWidget

---
+++ manifestme-frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import DailyAffirmationWidget from './DailyAffirmationWidget';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to ManifestMe</h1>
+      <DailyAffirmationWidget />
+    </div>
+  );
+}
+
+export default HomePage;
