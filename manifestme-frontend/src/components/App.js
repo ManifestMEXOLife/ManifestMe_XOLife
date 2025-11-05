1c946483f27f34659232c7fc8aaac2997d24ebf8
Rocket3012 <145717198+Rocket3012@users.noreply.github.com>
2025-11-05 08:20:35 -0700

    Create App component for ManifestMe frontend

---
+++ manifestme-frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HealthCheck from "./components/HealthCheck";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ManifestMe Frontend</h1>
+      <HealthCheck />
+    </div>
+  );
+}
+
+export default App;
