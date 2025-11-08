f19238a93e5e959781b075b0b543b1c13f9be868
Rocket3012 <145717198+Rocket3012@users.noreply.github.com>
2025-11-08 13:20:05 -0700

    Fix formatting in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,16 @@ module.exports = {
   apps: [
     {
       name: "manifestme-backend",
-      script: "./dist/server.js",
-      watch: false,
+      script: "./dist/server.js",  // points to the compiled JS
       instances: 1,
       autorestart: true,
+      watch: false,
       env: {
-        NODE_ENV: "development"
+        NODE_ENV: "development",
       },
       env_production: {
-        NODE_ENV: "production"
-      }
-    }
-  ]
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
