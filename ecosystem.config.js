d1ebbb4a6d82140724aca556959dd128b67e89c8
Rocket3012 <145717198+Rocket3012@users.noreply.github.com>
2025-11-08 13:21:07 -0700

    Modify ecosystem.config.js for development setup
    Updated the script and interpreter for development and production environments.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,21 @@ module.exports = {
   apps: [
     {
       name: "manifestme-backend",
-      script: "./dist/server.js",  // points to the compiled JS
+      // Use ts-node in development
+      script: "./server.ts",
       instances: 1,
       autorestart: true,
       watch: false,
+      interpreter: "ts-node",
+      interpreter_args: "-r tsconfig-paths/register",
       env: {
         NODE_ENV: "development",
       },
       env_production: {
         NODE_ENV: "production",
+        // Override script and interpreter for production
+        script: "./dist/server.js",
+        interpreter: "node",
       },
     },
   ],
