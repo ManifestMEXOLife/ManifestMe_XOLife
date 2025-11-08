4fb689270efc862f2614f1f4a15e6973e9f7b249
Rocket3012 <145717198+Rocket3012@users.noreply.github.com>
2025-11-08 12:28:30 -0700

    Modify ecosystem config for TypeScript and dev mode
    Updated the application configuration for development mode with TypeScript support.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,17 @@ module.exports = {
   apps: [
     {
       name: "manifestme-backend",
-      script: "dist/server.js",
-      instances: "max",
-      exec_mode: "cluster",
-      watch: false,
-      max_memory_restart: "500M",
+      script: "./server.ts",
+      interpreter: "ts-node",
+      watch: true,
       env: {
-        NODE_ENV: "production",
-        PORT: 8080,
+        NODE_ENV: "development",
+        PORT: 8080
       },
-    },
-  ],
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 8080
+      }
+    }
+  ]
 };
